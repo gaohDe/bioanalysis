0f2f97408d926f86e9ce891b9aa383ea909a6a29
gaohuan <779312688@qq.com>
2017-07-16 21:03:41 +0800

    登陆完善

--- bioanalysis/WebContent/pages/app/js/services/loginService.js
+++ bioanalysis/WebContent/pages/app/js/services/loginService.js
@@ -5,10 +5,8 @@ app.service("loginService", function ($http) {
     this.login = function (user) {
         var promise = $http({
             method: "post",
-            // url: ORIGINSERVERADDRESS + "/login_login",
-            url: LOCALSERVERADDRESS + "/login_login",
-            //headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-            data: {login_name:'admin',password:'admin',is_remember:'0'}
+            url: SERVERADDRESS + "/login_login",
+            data: user
         });
 
         return promise.then(function (res) {

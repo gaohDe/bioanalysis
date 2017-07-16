01f096aa999423d0becbd9f9f51cebcc7746cf41
gaohuan <779312688@qq.com>
2017-07-16 21:11:38 +0800

    完善登陆

--- bioanalysis/WebContent/pages/app/js/main.js
+++ bioanalysis/WebContent/pages/app/js/main.js
@@ -6,5 +6,5 @@ var app = angular.module('angle', [
     'oc.lazyLoad',
     'ngCookies'
 ]);
-var SERVERADDRESS = "http://111.231.15.108";
-// var SERVERADDRESS = "http://localhost:8080/bioanalysis";
+// var SERVERADDRESS = "http://111.231.15.108";
+var SERVERADDRESS = "http://localhost:8080/bioanalysis";
